8c38f28828b183a196b851c46e1e48a1c336b0c7
haruna <haruna0306@g.yju.ac.kr>
2025-08-13 23:30:19 +0900

    削除

---
+++ src/practice/board/delete.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>글 삭제</title>
+</head>
+<body>
+    <h1>글 삭제</h1>
+    <hr>
+    <form action="delete_process.php" method="post">
+        <fieldset>
+            비밀번호:
+            <input type="password" name="pw">
+            <button>삭제하기</button>
+        </fieldset>
+    </form>
+</body>
+</html>
