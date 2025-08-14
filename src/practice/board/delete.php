36f62c254a7feb828c51fe58db24110566ffa3c0
haruna <haruna0306@g.yju.ac.kr>
2025-08-14 21:59:18 +0900

    削除

--- src/practice/board/delete.php
+++ src/practice/board/delete.php
@@ -1,20 +1,34 @@
+<?php
+$id = isset($_GET['id']) ? intval($_GET['id']) : '';
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>글 삭제</title>
 </head>
+
 <body>
+
+        <!--
+        POST방식으로 pw를 받는다
+        -->
+
     <h1>글 삭제</h1>
     <hr>
     <form action="delete_process.php" method="post">
         <fieldset>
             비밀번호:
+            <input type="hidden" name="id" value="<?= $id ?>">
             <input type="password" name="pw">
             <button>삭제하기</button>
         </fieldset>
     </form>
 </body>
+
 </html>
